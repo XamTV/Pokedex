c625309a7d717865487e1bc353b4dba792be4055
Maxime MAUSSION <maximemaussion30@gmail.com>
2024-03-26 16:35:57 +0100

    Made modification in PokemonCard.jsx following the React Basics 03 Challenge

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,29 @@
+const PokemonList = [
+  {
+    pokemonName: "Bulbizarre",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    pokemonName: "Mew",
+  },
+];
+
 function PokemonCard() {
+  const pokemon = PokemonList[0]; // Replace 0 BY 1 To get Mew Instead
+
+  if (pokemon.pokemonName === "Bulbizarre") {
+    return (
+      <figure>
+        <img src={pokemon.imgSrc} alt={pokemon.pokemonName} />
+        <figcaption>{pokemon.pokemonName} </figcaption>
+      </figure>
+    );
+  }
   return (
     <figure>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="Bulbizarre"
-      />
-      <figcaption>Bulbizarre</figcaption>
+      <p>???</p>
+      <figcaption>{pokemon.pokemonName} </figcaption>
     </figure>
   );
 }
